2ababed3864ded521a816d82e6845585b7c2ba97
Bartusus1337 <bs88666@stud.uws.edu.pl>
2026-02-04 22:38:35 +0100

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// main.js - Tylko podstawowa inicjalizacja widoku
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Santa Dashboard Ready");
-    // Tutaj można dodać ewentualne dodatkowe animacje interfejsu
-});
